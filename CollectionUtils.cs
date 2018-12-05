57eafd284e305f325e1b2f44a8ce487ae98d15f7
Tomás Rinaldi <tomirinaldi8@gmail.com>
2018-12-04 22:22:46 -0300

    Added IEnumerable.ForEach extension method

---
+++ CollectionUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Tomi.Utils
+{
+
+    public static class CollectionUtils
+    {
+
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            if (action == null) return;
+
+            foreach (T item in enumerable) action(item);
+        }
+
+    }
+
+}
