481bba02d180219d60e0934b84b75f651e96fe7e
tomi901 <tomirinaldi8@gmail.com>
2020-02-21 14:29:26 -0300

    Transformed to package

--- MonoBehaviourUtils.cs
+++ MonoBehaviourUtils.cs
@@ -2,51 +2,42 @@
 using System.Collections;
 using UnityEngine;
 
-using Object = UnityEngine.Object;
-
 
 namespace Tomi.Utils
 {
     public static partial class UnityUtils
     {
-
-        public static void LogWithContext(this Object obj, string message)
-        {
-            Debug.Log(message, obj);
-        }
-
         public static Coroutine Invoke(this MonoBehaviour monoBehaviour, Action action, float time)
         {
-            return monoBehaviour.StartCoroutine(WaitForAction(action, time));
+            return monoBehaviour.StartCoroutine(Routine());
+
+            IEnumerator Routine()
+            {
+                if (action == null) yield break;
+
+                yield return new WaitForSeconds(time);
+                action.Invoke();
+            }
         }
 
         public static Coroutine InvokeRepeating(this MonoBehaviour monoBehaviour, Action action, float time, 
             float repeatTime)
         {
-            return monoBehaviour.StartCoroutine(WaitAndRepeatAction(action, time, repeatTime));
-        }
+            return monoBehaviour.StartCoroutine(Routine());
 
-        private static IEnumerator WaitForAction(Action action, float time)
-        {
-            if (action == null) yield break;
-
-            yield return new WaitForSeconds(time);
-            action.Invoke();
-        }
-
-        private static IEnumerator WaitAndRepeatAction(Action action, float time, float repeatTime)
-        {
-            if (action == null) yield break;
-
-            yield return new WaitForSeconds(time);
-
-            WaitForSeconds repeatWait = new WaitForSeconds(repeatTime);
-            while (true)
+            IEnumerator Routine()
             {
-                action.Invoke();
-                yield return repeatWait;
+                if (action == null) yield break;
+
+                yield return new WaitForSeconds(time);
+
+                WaitForSeconds repeatWait = new WaitForSeconds(repeatTime);
+                while (true)
+                {
+                    action.Invoke();
+                    yield return repeatWait;
+                }
             }
         }
-
     }
 }
