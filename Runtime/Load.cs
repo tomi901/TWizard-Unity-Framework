1980aec866250e0b3d779ba22f19ed510036cdba
Tomás Rinaldi <tomirinaldi8@gmail.com>
2020-07-22 18:35:52 -0300

    Replaced LoadingScreenManager with Load

---
+++ Runtime/Load.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+namespace TWizard.Framework
+{
+    /// <summary>
+    /// Util static class to allow the await of unity async operations.
+    /// </summary>
+    public static class Load
+    {
+        #region AsyncOperation await
+        public struct AsyncOperationAwaiter : INotifyCompletion
+        {
+            public AsyncOperation operation;
+
+            public bool IsCompleted => operation.isDone;
+
+            public AsyncOperationAwaiter(AsyncOperation operation)
+            {
+                this.operation = operation;
+            }
+
+            public void OnCompleted(Action continuation)
+            {
+                operation.completed += (op) => continuation();
+            }
+
+            public AsyncOperation GetResult() => operation;
+        }
+
+        public static AsyncOperationAwaiter GetAwaiter(this AsyncOperation operation) => new AsyncOperationAwaiter(operation);
+        #endregion
+
+
+        private static Scene lastLoadedScene;
+
+        static Load()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            lastLoadedScene = scene;
+        }
+
+
+        public delegate Task TaskOperation();
+
+        public static async Task<Scene> Scene(string sceneName, LoadSceneMode mode = LoadSceneMode.Single,
+            TaskOperation postLoad = null)
+        {
+            await SceneManager.LoadSceneAsync(sceneName, mode);
+            Scene loadedScene = lastLoadedScene;
+
+            if (postLoad != null)
+            {
+                // Deactivate the objects to allow post load
+                var sceneObjects = new List<GameObject>();
+                loadedScene.GetRootGameObjects(sceneObjects);
+                sceneObjects.RemoveAll(go => !go.activeSelf);
+
+                foreach (GameObject obj in sceneObjects)
+                    obj.SetActive(false);
+
+                // Do all the post-load operations while awaiting them
+                foreach (TaskOperation operation in postLoad.GetInvocationList())
+                    await operation();
+
+                // Activate the root objects that were deactivated
+                foreach (GameObject obj in sceneObjects)
+                    obj.SetActive(true);
+            }
+            return loadedScene;
+        }
+    }
+}
