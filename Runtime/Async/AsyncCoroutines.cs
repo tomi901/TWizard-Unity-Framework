01baaff15626fa96dac36ea2b53c6a276149a43e
Tomás Rinaldi <tomirinaldi8@gmail.com>
2020-12-19 18:29:32 -0300

    Coroutines and IEnumerators are awaitable

---
+++ Runtime/Async/AsyncCoroutines.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace TWizard.Core.Async
+{
+    public static class AsyncCoroutines
+    {
+        public static Task AsTask(this Coroutine coroutine) => UnityEvents.Instance.AwaitAsCoroutine(coroutine);
+        public static TaskAwaiter GetAwaiter(this Coroutine coroutine) => UnityEvents.Instance.AwaitAsCoroutine(coroutine).GetAwaiter();
+
+
+        public static Task AsTask(this IEnumerator enumerator) => UnityEvents.Instance.AwaitAsCoroutine(enumerator);
+
+        public static TaskAwaiter GetAwaiter(this IEnumerator enumerator) => UnityEvents.Instance.AwaitAsCoroutine(enumerator).GetAwaiter();
+    }
+}
