80f996f257e2e08538d9e5bbe773b30fcb367d0d
Tomás Rinaldi <tomirinaldi8@gmail.com>
2020-12-19 18:23:06 -0300

    Replaced AwaitCoroutine with UnityEvents. WIP

---
+++ Runtime/Async/UnityEvents.cs
@@ -0,0 +1,72 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+namespace TWizard.Core.Async
+{
+    [HideInInspector]
+    public sealed class UnityEvents : MonoBehaviour
+    {
+        private static UnityEvents instance;
+        internal static UnityEvents Instance
+        {
+            get
+            {
+                if (instance)
+                {
+                    var go = new GameObject("Unity Events")
+                    {
+                        hideFlags = HideFlags.HideAndDontSave
+                    };
+                    instance = go.AddComponent<UnityEvents>();
+                }
+                return instance;
+            }
+        }
+
+        public enum Type
+        {
+            Update,
+            FixedUpdate,
+        }
+
+
+        public static event UnityAction OnUpdate { add => Instance.onUpdate += value; remove => Instance.onUpdate -= value; }
+        public static event UnityAction OnFixedUpdate { add => Instance.onFixedUpdate += value; remove => Instance.onFixedUpdate -= value; }
+
+
+        private UnityAction onUpdate = delegate { },
+            onFixedUpdate = delegate { };
+
+
+        private void Update()
+        {
+            onUpdate();
+        }
+
+        private void FixedUpdate()
+        {
+            onFixedUpdate();
+        }
+
+
+
+        public Task AwaitAsCoroutine(System.Collections.IEnumerator enumerator) => AwaitAsCoroutine(enumerator);
+
+        public Task AwaitAsCoroutine(YieldInstruction instruction) => AwaitAsCoroutine(instruction);
+
+        public Task AwaitAsCoroutine(object obj)
+        {
+            var task = new TaskCompletionSource<bool>();
+
+            StartCoroutine(Routine());
+            System.Collections.IEnumerator Routine()
+            {
+                yield return obj;
+                task.SetResult(true);
+            }
+            return task.Task;
+        }
+    }
+}
