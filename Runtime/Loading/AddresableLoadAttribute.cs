1ba95f2594c40c6832a7ac58532d7ed26892bac9
Tomás Rinaldi <tomirinaldi8@gmail.com>
2020-12-24 01:28:53 -0300

    Added AddresableLoad to allow loading as adressable with AssetLoad

---
+++ Runtime/Loading/AddresableLoadAttribute.cs
@@ -0,0 +1,34 @@
+﻿#if USING_ADDRESABLES
+using System;
+using UnityEngine.AddressableAssets;
+
+
+namespace TWizard.Core.Loading
+{
+    public class AddresableLoadAttribute : AssetLoadAttribute
+    {
+        public string Key { get; }
+
+        public AddresableLoadAttribute(string key) => Key = key;
+
+
+        public override T Load<T>()
+        {
+            var operation = Addressables.LoadAssetAsync<T>(Key);
+            operation.Task.Wait();
+            return operation.Result;
+        }
+
+        public override void LoadAsync<T>(Action<T> onLoaded, Action<Exception> onError)
+        {
+            Addressables.LoadAssetAsync<T>(Key).Completed += (op) =>
+            {
+                if (op.OperationException != null)
+                    onError(op.OperationException);
+                else
+                    onLoaded(op.Result);
+            };
+        }
+    }
+}
+#endif
