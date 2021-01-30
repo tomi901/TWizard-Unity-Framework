12ef6d0d18fd70fa0769538b6960cbcd4b247d30
Tomás Rinaldi <tomirinaldi8@gmail.com>
2021-01-29 23:34:17 -0300

    Added generic resource load method

---
+++ Runtime/Loading/LoadResource.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+#if UNITASK
+using Cysharp.Threading.Tasks;
+#endif
+
+
+namespace TWizard.Core.Loading
+{
+    public static partial class Load
+    {
+#if UNITASK
+        public static async UniTask<T> Resource<T>(string path, System.IProgress<float> progress = null, PlayerLoopTiming playerLoopTiming = PlayerLoopTiming.Update)
+            where T : Object
+        {
+            var request = Resources.LoadAsync<T>(path);
+            await request.ToUniTask(progress, playerLoopTiming);
+            return (T)request.asset;
+        }
+#endif
+    }
+}
