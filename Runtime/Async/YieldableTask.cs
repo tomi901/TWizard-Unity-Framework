e561964bf687a9d0c4ff1a3fb5ccb4c17be58840
Tomás Rinaldi <tomirinaldi8@gmail.com>
2021-02-02 21:08:40 -0300

    Added ResultListener for coroutines

--- Runtime/Async/YieldableTask.cs
+++ Runtime/Async/YieldableTask.cs
@@ -26,7 +26,9 @@ namespace TWizard.Core.Async
         public YieldableTask(Task<T> task) => Task = task ?? throw new ArgumentNullException(nameof(task));
     }
 
-
+    /// <summary>
+    /// Util yieldable class to yield until <see cref="Finish"/> is called, can be reused with <see cref="Reset"/>.
+    /// </summary>
     public class YieldSignal : CustomYieldInstruction
     {
         private bool wait = true;
@@ -37,15 +39,39 @@ namespace TWizard.Core.Async
         public new void Reset() => wait = true;
     }
 
+    /// <summary>
+    /// Util yieldable class to yield a method with a <see cref="ResultCallback{T}"/>, can be used as a parameter of that type..
+    /// </summary>
+    /// <typeparam name="T">The result type.</typeparam>
+    public class ResultListener<T> : CustomYieldInstruction
+    {
+        public override bool keepWaiting => !result.HasValue;
+
+        private Result<T>? result = null;
+        private Result<T> Result => result ?? throw new NullReferenceException("Result is not set yet.");
+        public bool HasResult => result.HasValue;
+
+        public T Value => Result.Value;
+
+
+        public void SetResult(Result<T> result) => this.result = result;
+        public void SetResult(T result) => this.result = new Result<T>(result);
+        public void SetException(Exception exception) => result = new Result<T>(exception);
+
+        public void ThrowIfException()
+        {
+            var result = Result;
+            if (result.HasError)
+                throw result.Exception;
+        }
+
+
+        public static implicit operator ResultCallback<T>(ResultListener<T> listener) => listener.SetResult;
+    }
+
 
     public static class Yield
     {
-        /// <summary>
-        /// Creates a new yieldable signal, complete it with <see cref="YieldSignal.Finish"/>.
-        /// </summary>
-        public static YieldSignal Signal => new YieldSignal();
-
-
         public static bool ShouldKeepWaiting(this Task task) => !task.IsCompleted || !task.IsFaulted || !task.IsCanceled;
 
 
