74e3606f8ddafe5a64bb3ad61ec85e25bd879288
Tomás Rinaldi <tomirinaldi8@gmail.com>
2021-02-02 21:09:34 -0300

    Added cancellationToken to resource load

--- Runtime/Loading/LoadResource.cs
+++ Runtime/Loading/LoadResource.cs
@@ -1,5 +1,5 @@
-using UnityEngine;
 #if UNITASK
+using UnityEngine;
 using Cysharp.Threading.Tasks;
 #endif
 
@@ -9,11 +9,12 @@ namespace TWizard.Core
     public static partial class Load
     {
 #if UNITASK
-        public static async UniTask<T> Resource<T>(string path, System.IProgress<float> progress = null, PlayerLoopTiming playerLoopTiming = PlayerLoopTiming.Update)
+        public static async UniTask<T> Resource<T>(string path, System.IProgress<float> progress = null, PlayerLoopTiming playerLoopTiming = PlayerLoopTiming.Update,
+            System.Threading.CancellationToken cancellationToken = default)
             where T : Object
         {
             var request = Resources.LoadAsync<T>(path);
-            await request.ToUniTask(progress, playerLoopTiming);
+            await request.ToUniTask(progress, playerLoopTiming, cancellationToken);
             return (T)request.asset;
         }
 #endif
